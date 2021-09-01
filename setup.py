4e2bc4c43aa1cf437595594e44ce3103ae3d2c91
tov101 <ov.mihaila@gmail.com>
2021-09-01 21:50:20 +0300

    Update license.

--- setup.py
+++ setup.py
@@ -88,7 +88,7 @@ setup(
         'Topic :: Debug :: Python GUI Apps',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: ',
+        'License :: Public Domain',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
